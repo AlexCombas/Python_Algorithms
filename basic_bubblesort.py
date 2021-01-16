eb38935dd8c393a9a8e1df8ce1dcde7e756f2a03
Alex Combas <alex@flyingcastle.net>
2021-01-15 20:52:38 -0800

    added a more efficient version of bubble sort

--- basic_bubblesort.py
+++ basic_bubblesort.py
@@ -6,6 +6,24 @@ def bubble_sort_1(list_a):
                 list_a[j], list_a[j + 1] = list_a[j + 1], list_a[j]
 
 
-a = [10, 8, 3, 6, 1]
-bubble_sort_1(a)
-print(a)
+a_1 = [10, 8, 3, 16, 2, 9, 7, 10, 2, 12, 22, 11, 6, 1]
+bubble_sort_1(a_1)
+print(a_1)
+
+
+def bubble_sort_2(list_a):
+    n = len(list_a)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if list_a[j] > list_a[j+1]:
+                list_a[j], list_a[j+1] = list_a[j+1], list_a[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+
+a_2 = [10, 8, 3, 16, 2, 9, 7, 10, 2, 12, 22, 11, 6, 1]
+bubble_sort_1(a_2)
+print(a_2)
