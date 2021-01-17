4605a8f43c52bfa523b2384db356319ea8d15b82
Alex Combas <alex@flyingcastle.net>
2021-01-16 22:48:39 -0800

    added second selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,5 @@
 def selection_sort(a_list):
-    for i in range(len(a_list)):
+    for i in range(len(a_list)-1):
 
         minimum_index = i
         for current_index in range(i+1, len(a_list)):
@@ -7,3 +7,25 @@ def selection_sort(a_list):
                 minimum_index = current_index
 
         a_list[i], a_list[minimum_index] = a_list[minimum_index], a_list[i]
+
+
+a = [16, 21, 12, 38, 23]
+selection_sort(a)
+print(a)
+
+
+def selection_sort_2(a_list):
+    for i in range(len(a_list)-1, 0, -1):
+        max_position = 0
+        for j in range(1, i+1):
+            if a_list[j] > a_list[max_position]:
+                max_position = j
+        temp = a_list[i]
+        a_list[i] = a_list[max_position]
+        a_list[max_position] = temp
+
+
+a2 = [84, 21, 96, 15, 47]
+print(a2)
+selection_sort_2(a2)
+print(a2)
