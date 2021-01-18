a4aa936734f7030e9b4fc589010b0e416dbbf7a9
Alex Combas <alex@flyingcastle.net>
2021-01-17 21:25:09 -0800

    formatting

--- insertion_sort.py
+++ insertion_sort.py
@@ -18,9 +18,9 @@ def insertion_sort_2(a_list):
     for i in range(1, len(a_list)):
         value = a_list[i]
         position = i
-        while position > 0 and a_list[position -1] > value:
-            a_list[position] = a[position -1]
-            position = position -1
+        while position > 0 and a_list[position - 1] > value:
+            a_list[position] = a[position - 1]
+            position = position - 1
 
         a[position] = value
 
