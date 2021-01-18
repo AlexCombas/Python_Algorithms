4b7ca30811c9914a475b4557a5b250d227a69ef1
Alex Combas <alex@flyingcastle.net>
2021-01-17 21:22:18 -0800

    added second version of insertion sort, basically the same thing

--- insertion_sort.py
+++ insertion_sort.py
@@ -12,3 +12,20 @@ def insertion_sort(a_list):
 a = [10, 3, 6, 8, 2]
 insertion_sort(a)
 print(a)
+
+
+def insertion_sort_2(a_list):
+    for i in range(1, len(a_list)):
+        value = a_list[i]
+        position = i
+        while position > 0 and a_list[position -1] > value:
+            a_list[position] = a[position -1]
+            position = position -1
+
+        a[position] = value
+
+
+a = [84, 21, 96, 15, 47]
+print(a)
+insertion_sort_2(a)
+print(a)
